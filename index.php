b0a7a171e644213595fb782ae76b50f3de50d77f
MaaikeMassarotto <domyna@live.nl>
2024-12-08 21:14:31 +0100

    Add files via upload
    alleen de blog

---
+++ index.php
@@ -0,0 +1,208 @@
+<?php
+require '../scripts/Collection.php';
+Scripts\Collection::setSortRandomSeed();
+?><!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>Blog - Margret IJdema</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta property="og:title" content="Blog">
+<meta property="og:type" content="website">
+<meta property="og:updated_time" content="1733223304">
+<meta name="twitter:title" content="Blog">
+        <link rel="shortcut icon" href="data:;base64,iVBORw0KGgo=">
+        <link rel="canonical" href="https://www.margret-ijdema.nl/blog/">
+        <link rel="preload" href="/assets/fonts/open-sans/open-sans-light.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="preload" href="/assets/fonts/open-sans/open-sans-semibold.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="preload" href="/assets/fonts/open-sans/open-sans-regular.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="preload" href="/assets/fonts/roboto/roboto-medium.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="stylesheet" href="/assets/css/fonts.css?v=1733223304">
+        <link rel="stylesheet" href="/assets/css/website.css?v=1733223304">
+        <link rel="stylesheet" href="/assets/css/page-977247.css?v=1733223304">
+        <script>
+    const LANGUAGE_CODE = 'nl';
+</script>
+<script src="/assets/vendors/jquery/jquery.min.js"></script>
+        
+        
+        
+        <script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date(); tElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60596621-1', 'auto');
+  ga('send', 'pageview');
+</script>
+    </head>
+    <body>
+        
+        
+        <div class="page" id="page-977247">
+		<header class="block-1-1 v-35">
+			<div class="container container-1">
+				<div class="row row-1">
+					<div class="col col-1">
+						<div class="media-1" data-effect="zoom-in">
+							<img src="/assets/files/tekengebied-1.png" srcset="/assets/files/tekengebied-1.480x0x0x100.png 480w, /assets/files/tekengebied-1.960x0x0x100.png 960w, /assets/files/tekengebied-1.1920x0x0x100.png 1920w, /assets/files/tekengebied-1.png 1921w" sizes="(max-width: 480px) 480px, (max-width: 960px) 960px, (max-width: 1920px) 1920px, 1921px" alt>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<header class="block-5-2 v-34" data-sticky="sticky">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="menu">
+							<a class="handler" href="#">
+								<div></div>
+								<div></div>
+								<div></div>
+							</a>
+							<div class="block-5-2-mobile-menu mobile-menu v-34">
+								<div class="outer">
+									<div class="inner">
+										<div class="menu-1" data-levels="2">
+											<ul><li class="page-255836"><a href="/">Home</a></li><li class="page-255874"><a href="/over-margret/">Over Margret</a></li><li class="page-255879"><a href="/paratriathlon/">Paratriathlon</a></li><li class="page-256084"><a href="/het-team/">Het team</a></li><li class="page-256099"><a href="/sponsors/">Sponsors</a></li><li class="active page-977247"><a href="/blog/">Blog</a></li><li class="page-256100"><a href="/nieuws/">Nieuws</a></li></ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<section id="block-29-9776408" class="v-34">
+			<div class="media-4" data-size="1920x0">
+				<img src="/assets/files/header-zwembad2.1920x0.png" srcset="/assets/files/header-zwembad2.480x0.png 480w, /assets/files/header-zwembad2.960x0.png 960w, /assets/files/header-zwembad2.1920x0.png 1920w" sizes="(max-width: 480px) 480px, (max-width: 960px) 960px, 1920px" alt>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2 empty">
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="block-48-1 v-34">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="middle">
+							<div class="social">
+								<a class="media-1" href="https://www.facebook.com/margret.ijdema.5" target="_blank" id="media-1-d7igg3t4">
+									<img src="/assets/files/facebook-1.ff8800.svg" alt>
+								</a>
+							</div>
+							<div class="content">
+								<div class="media-1" id="media-1-hjpoyzf0">
+									<img src="/assets/files/local-phone.ff8800.svg" alt>
+								</div>
+								<div class="text-6"><p><span style="color: #000000;"></span>06 25 09 89 96</p></div>
+								<div class="media-1">
+									<img src="/assets/files/email.ff8800.svg" alt>
+								</div>
+								<div class="text-6"><p><em></em><a href="mailto:&#104;&#101;&#114;&#98;&#121;&#108;&#111;&#98;&#101;&#108;&#108;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;"><span style="text-decoration: underline;">&#104;&#101;&#114;&#98;&#121;&#108;&#111;&#98;&#101;&#108;&#108;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;</span></a></p></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="block-9-9776403" class="v-34">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<h1 class="heading-2 has-sub">Welkom bij mij blog</h1>
+						<h2 class="heading-3">Met een ondertitel</h2>
+						<div class="text-2">
+							<p style="text-align: left;">&ldquo;Ik doe de dingen op mijn manier, zoals het voor mij goed is. Dat is soms niet de manier zoals het &lsquo;hoort&rsquo; of voor anderen werkt, niet het meest logische. Dat heb ik moeten leren en durven accepteren, maar nu ik dat kan, gaat het ontzettend hard.&rdquo;</p>
+							<p style="text-align: left;">Mijn blogs zijn in samenwerking met <a href="https://thrivingmagazine.nl" target="_blank"><span style="color: #4169E1;">Thriving magazine</span> </a>tot stand gekomen</p>
+						</div>
+						<div class="text-3">
+							<p>Lorem ipsum dolor sit amet, consectetur adipis cin elit. Nunc purus libero, interdum sed blandit acp retium facilisis turpis. Donec dictum neque veloran tristique egestas nulla mollis dui lorem dolor. Lorem ipsum dolor sit amet, consectetur adipis cin elit. Nunc purus libero, interdum sed blandit acp retium facilisis turpis. Donec dictum neque veloran tristique egestas nulla mollis dui lorem dolor. Lorem ipsum dolor sit amet, consectetur adipis cin elit. Nunc purus libero, interdum sed blandit acp retium facilisis turpis. Donec dictum neque veloran tristique egestas nulla mollis dui lorem dolor. Lorem ipsum dolor sit amet, consectetur adipis cin elit. Nunc purus libero, interdum sed blandit acp retium facilisis turpis. Donec dictum neque veloran tristique egestas nulla mollis dui lorem dolor.</p>
+						</div>
+						<div class="button-2">Meer lezen</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="block-14-9776412" class="v-35">
+			<div class="container container-1" data-collection="9776412"><?php echo Scripts\Collection::getHtml(29200, 'block-14-9776412', '
+				<article class="row row-1">
+					<div class="col col-1">
+						<div class="media-3" data-size="480x0" data-ratio="1x1">
+							<img src="/assets/files/image-065.480x0.jpg" srcset="/assets/files/image-065.480x0.jpg 480w, /assets/files/image-065.960x0.jpg 960w" sizes="480px" alt>
+						</div>
+					</div>
+					<div class="col col-2">
+						<div class="box">
+							<h3 class="heading-4 has-sub">Nieuwsbericht 1</h3>
+							<h4 class="heading-5">31 maart 2023</h4>
+							<div class="text-4">
+								<p>Lorem ipsum dolor sit amet, consectetur adipis cin elit. Nunc purus libero, interdum sed blandit acp retium facilisis turpis. Donec dictum neque veloran tristique egestas nulla mollis dui lorem dolor.</p>
+							</div>
+							<div class="button-wrapper">
+								<div class="button-5">Meer lezen</div>
+							</div>
+						</div>
+					</div>
+				</article>', ['items_per_page' => 24], 0, null, 977247, null, null); ?>
+				<div class="row row-2">
+					<div class="col col-3 col-sm-offset-5 col-md-offset-3 col-lg-offset-2 empty">
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer id="block-12-9776409" class="v-34">
+			<div class="container">
+				<div class="row">
+					<div class="col col-md-4">
+						<div class="media-1">
+							<img src="/assets/files/tekengebied-1.png" srcset="/assets/files/tekengebied-1.480x0x0x100.png 480w, /assets/files/tekengebied-1.960x0x0x100.png 960w, /assets/files/tekengebied-1.1920x0x0x100.png 1920w, /assets/files/tekengebied-1.png 1921w" sizes="(max-width: 480px) 480px, (max-width: 960px) 960px, (max-width: 1920px) 1920px, 1921px" alt>
+						</div>
+					</div>
+					<div class="col col-md-4">
+						<div class="text-6">
+							<p>Larixplantsoen 4<br />1702 XH Heerhugowaard</p>
+							<p>Banknummer: <br />NL44 RABO 0326936211</p>
+						</div>
+					</div>
+					<div class="col col-md-4">
+						<div class="text-6">
+							<p>+31 625 098 996<br><a href="mailto: &#104;&#101;&#114;&#98;&#121;&#108;&#111;&#98;&#101;&#108;&#108;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;"><span style="color: #ffffff;">&#104;&#101;&#114;&#98;&#121;&#108;&#111;&#98;&#101;&#108;&#108;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;</span></a></p>
+							<p>&copy; 2024 ~ <span style="color: #ffffff;"><a href="https://mb-ictbeheer.nl" target="_blank">MB-ICTbeheer</a></span><span style="text-decoration: underline;"><br><br></span><br><span style="text-decoration: underline;"></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+        <script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60596621-1', 'auto');
+  ga('send', 'pageview');
+</script>
+        <script src="/assets/js/script.js?v=1733223304"></script>
+<script>
+    window.addEventListener('load', () => {
+        new Video();
+new Sticky();
+new Effect();
+new Popin();
+new Collection();
+
+new Menu({selector: '.block-5-2 .menu'});
+
+window.parent.postMessage({'height': document.body.scrollHeight});
+    });
+</script>
+    </body>
+</html>
---
+++ template.php
@@ -0,0 +1,165 @@
+<?php
+require '../scripts/Collection.php';
+Scripts\Collection::getItemError(29200, '../404.php', null);
+?><!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <?php echo Scripts\Collection::getItemMetaHtml(29200, null, ' - Margret IJdema', ); ?>
+        <link rel="shortcut icon" href="data:;base64,iVBORw0KGgo=">
+        <?php echo Scripts\Collection::getCanonicalHtml(29200, null, 'https://www.margret-ijdema.nl'); ?>
+        <link rel="preload" href="/assets/fonts/open-sans/open-sans-light.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="preload" href="/assets/fonts/open-sans/open-sans-semibold.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="preload" href="/assets/fonts/open-sans/open-sans-regular.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="preload" href="/assets/fonts/roboto/roboto-medium.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+<link rel="stylesheet" href="/assets/css/fonts.css?v=1733223304">
+        <link rel="stylesheet" href="/assets/css/website.css?v=1733223304">
+        <link rel="stylesheet" href="/assets/css/page-977245.css?v=1733223304">
+        <script>
+    const LANGUAGE_CODE = 'nl';
+</script>
+<script src="/assets/vendors/jquery/jquery.min.js"></script>
+        
+        
+        
+        <script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date(); tElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60596621-1', 'auto');
+  ga('send', 'pageview');
+</script>
+    </head>
+    <body>
+        
+        
+        <div class="page" id="page-977245">
+		<header class="block-1-1 v-35">
+			<div class="container container-1">
+				<div class="row row-1">
+					<div class="col col-1">
+						<div class="media-1" data-effect="zoom-in">
+							<img src="/assets/files/tekengebied-1.png" srcset="/assets/files/tekengebied-1.480x0x0x100.png 480w, /assets/files/tekengebied-1.960x0x0x100.png 960w, /assets/files/tekengebied-1.1920x0x0x100.png 1920w, /assets/files/tekengebied-1.png 1921w" sizes="(max-width: 480px) 480px, (max-width: 960px) 960px, (max-width: 1920px) 1920px, 1921px" alt>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<header class="block-5-2 v-34" data-sticky="sticky">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="menu">
+							<a class="handler" href="#">
+								<div></div>
+								<div></div>
+								<div></div>
+							</a>
+							<div class="block-5-2-mobile-menu mobile-menu v-34">
+								<div class="outer">
+									<div class="inner">
+										<div class="menu-1" data-levels="2">
+											<ul><li class="page-255836"><a href="/">Home</a></li><li class="page-255874"><a href="/over-margret/">Over Margret</a></li><li class="page-255879"><a href="/paratriathlon/">Paratriathlon</a></li><li class="page-256084"><a href="/het-team/">Het team</a></li><li class="page-256099"><a href="/sponsors/">Sponsors</a></li><li class="page-977247"><a href="/blog/">Blog</a></li><li class="page-256100"><a href="/nieuws/">Nieuws</a></li></ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<section id="block-29-9776415" class="v-34">
+			<div class="media-4" data-size="1920x0">
+				<img src="/assets/files/header-zwembad2.1920x0.png" srcset="/assets/files/header-zwembad2.480x0.png 480w, /assets/files/header-zwembad2.960x0.png 960w, /assets/files/header-zwembad2.1920x0.png 1920w" sizes="(max-width: 480px) 480px, (max-width: 960px) 960px, 1920px" alt>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2 empty">
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="block-48-1 v-34">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="middle">
+							<div class="social">
+								<a class="media-1" href="https://www.facebook.com/margret.ijdema.5" target="_blank" id="media-1-d7igg3t4">
+									<img src="/assets/files/facebook-1.ff8800.svg" alt>
+								</a>
+							</div>
+							<div class="content">
+								<div class="media-1" id="media-1-hjpoyzf0">
+									<img src="/assets/files/local-phone.ff8800.svg" alt>
+								</div>
+								<div class="text-6"><p><span style="color: #000000;"></span>06 25 09 89 96</p></div>
+								<div class="media-1">
+									<img src="/assets/files/email.ff8800.svg" alt>
+								</div>
+								<div class="text-6"><p><em></em><a href="mailto:&#104;&#101;&#114;&#98;&#121;&#108;&#111;&#98;&#101;&#108;&#108;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;"><span style="text-decoration: underline;">&#104;&#101;&#114;&#98;&#121;&#108;&#111;&#98;&#101;&#108;&#108;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;</span></a></p></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="block-9-9776417" class="v-34">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<?php echo Scripts\Collection::getItemFieldHtml(29200, 'pagina-titel', 'heading-2', '<h1 class="heading-2"><span style="font-size: 28px;">Eerst moeder, nu ook sportvrouw. </span><br><span style="font-size: 28px;">Doet dingen op haar manier.</span></h1>', null); ?>
+						<h2 class="heading-3">Met een ondertitel</h2>
+						<?php echo Scripts\Collection::getItemFieldHtml(29200, 'pagina-tekst', 'text-2', '<div class="text-2">
+							<p>Maak kennis met Margret IJdema, Nederlands beste vrouwelijke paratriatleet en de mondiale nummer 3 in haar tak van sport. Een vrouw die sportief gezien richting &eacute;&eacute;n groot doel werkt: een Olympische medaille, liefst in de kleur goud. Leef<span style="background-color: transparent;">&nbsp;mee met het intensieve traject dat haar naar de Paralympische triathlon in Parijs moet brengen.</span></p>
+						</div>', null); ?>
+						<div class="text-3">
+							<p>Lorem ipsum dolor sit amet, consectetur adipis cin elit. Nunc purus libero, interdum sed blandit acp retium facilisis turpis. Donec dictum neque veloran tristique egestas nulla mollis dui lorem dolor. Lorem ipsum dolor sit amet, consectetur adipis cin elit. Nunc purus libero, interdum sed blandit acp retium facilisis turpis. Donec dictum neque veloran tristique egestas nulla mollis dui lorem dolor. Lorem ipsum dolor sit amet, consectetur adipis cin elit. Nunc purus libero, interdum sed blandit acp retium facilisis turpis. Donec dictum neque veloran tristique egestas nulla mollis dui lorem dolor. Lorem ipsum dolor sit amet, consectetur adipis cin elit. Nunc purus libero, interdum sed blandit acp retium facilisis turpis. Donec dictum neque veloran tristique egestas nulla mollis dui lorem dolor.</p>
+						</div>
+						<div class="button-2">Meer lezen</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer class="block-12-3 v-34">
+			<div class="container">
+				<div class="row">
+					<div class="col col-md-4">
+						<div class="media-1">
+							<img src="/assets/files/tekengebied-1.png" srcset="/assets/files/tekengebied-1.480x0x0x100.png 480w, /assets/files/tekengebied-1.960x0x0x100.png 960w, /assets/files/tekengebied-1.1920x0x0x100.png 1920w, /assets/files/tekengebied-1.png 1921w" sizes="(max-width: 480px) 480px, (max-width: 960px) 960px, (max-width: 1920px) 1920px, 1921px" alt>
+						</div>
+					</div>
+					<div class="col col-md-4">
+						<div class="text-6">
+							<p>Larixplantsoen 4<br>1702 XH Heerhugowaard</p>
+							<p>Ibannummer: <br>NL44 RABO 0326 936211</p>
+						</div>
+					</div>
+					<div class="col col-md-4">
+						<div class="text-6">
+							<p>+31 625 098 996<br><a href="mailto: &#104;&#101;&#114;&#98;&#121;&#108;&#111;&#98;&#101;&#108;&#108;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;"><span style="color: #ffffff;">&#104;&#101;&#114;&#98;&#121;&#108;&#111;&#98;&#101;&#108;&#108;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;</span></a></p>
+							<p>&copy; 2024 ~ <span style="color: #ffffff;"><a href="https://mb-ictbeheer.nl" target="_blank">MB-ICTbeheer</a></span><span style="text-decoration: underline;"><br><br></span><br><span style="text-decoration: underline;"></span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+        
+        <script src="/assets/js/script.js?v=1733223304"></script>
+<script>
+    window.addEventListener('load', () => {
+        new Video();
+new Sticky();
+new Effect();
+new Popin();
+
+new Menu({selector: '.block-5-2 .menu'});
+
+window.parent.postMessage({'height': document.body.scrollHeight});
+    });
+</script>
+    </body>
+</html>
